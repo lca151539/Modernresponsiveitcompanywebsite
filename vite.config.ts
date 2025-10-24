1a238f41f0300939b82616d2b8d4d5b817a9b99c
Livio Canobbio <canobbio@puzzle.ch>
2025-10-24 10:52:06 +0200

    feat: add build file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import { viteSingleFile} from 'vite-plugin-singlefile'
 
   export default defineConfig({
-    plugins: [react()],
+      base: './', // <<--- ADD THIS LINE
+    plugins: [react(), viteSingleFile()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
